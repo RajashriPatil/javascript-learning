06539310ff746cba0a0fe3230a4a860e5e981bb7
Rajashri Patil <rajashrivp432@gmail.com>
2024-02-14 19:06:29 +0000

    basics

--- basics/conversionOperations.js
+++ basics/conversionOperations.js
@@ -1,4 +1,4 @@
-let score=33;
+/* let score=33;
 console.log(typeof score); // Number
 console.log(typeof(score))// Number
 score="33aab" 
@@ -34,7 +34,59 @@ console.log(booleanval); // true
 let someNumber=33
 let stringnumber=Stirng(someNumber)
 console.log(stringnumber);// "33"
-console.log(typeof(stringnumber));// string
+console.log(typeof(stringnumber));// string */
+
+// ******************** Operations *******************
+
+let value=3
+let negvalue=-value
+console.log(negvalue); //-3
+console.log(2+2); // 4
+console.log(2-2); // 0
+console.log(2*2); // 4
+console.log(2/2); // 1
+console.log(2%2); // 0
+console.log(2**2); // 4
+
+let str1="hello"
+let str2=" Raj"
+
+let str3= str1+str2
+console.log(str3); // hello Raj
+
+console.log("1" +2); // 12
+console.log(1+"2"); // 12
+console.log("1"+2+2); // 122
+console.log(1+2+"2"); // 32
+
+console.log(true)// true
+console.log(+true)//1
+console.log(+"")// 0
+// console.log(true+) // error
+
+let num1,num2,num3;
+num1=num2=num3=2+2 // dont make tricky | not good
+
+let gameCounter=100
+gameCounter++
+console.log(gameCounter) //101
+++gameCounter
+console.log(gameCounter)// 102
+let val=gameCounter++
+console.log(gameCounter) // 103
+console.log(val)// 102
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
